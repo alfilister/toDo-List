72e185091dfb495d4edfa8e890ae6bf6dc5cff19
alfilister <castillo.c.jm@gmail.com>
2022-06-08 02:08:50 -0500

    SweetAlert problem solved

--- Client/src/Components/AddBar.jsx
+++ Client/src/Components/AddBar.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addTask } from "../Redux/Actions";
+import Swal from "sweetalert";
+
+const validate = (input) => {
+  let errors = {};
+  if (input.task.trim().length < 1) {
+    errors.task = "Task needed to add";
+  } else if (!/^.{0,50}$/.test(input.task)) {
+    errors.task = "Max length 50 characters";
+  }
+  return errors;
+};
 
 function AddBar() {
   const dispatch = useDispatch();
@@ -10,21 +21,37 @@ function AddBar() {
     priority: "Priority",
   });
 
+  const [errors, setErrors] = useState("");
+
   const handleInput = (e) => {
     e.preventDefault();
     setInput({
       ...input,
       [e.target.name]: e.target.value,
     });
+    setErrors(
+      validate({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
   };
 
   const handleBtn = (e) => {
     e.preventDefault();
-    dispatch(addTask({ task: input.task, priority: input.priority }));
-    setInput({
-      task: "",
-      priority: "Priority",
-    });
+    if (!input.task || input.priority === "Priority" || errors.task) {
+      Swal(
+        "Missing info",
+        "Set an Activity name and a Priority to proceed",
+        "warning"
+      );
+    } else {
+      dispatch(addTask({ task: input.task.trim(), priority: input.priority }));
+      setInput({
+        task: "",
+        priority: "Priority",
+      });
+    }
   };
 
   return (
@@ -33,9 +60,10 @@ function AddBar() {
         type="text"
         name="task"
         value={input.task}
-        placeholder="Type your activities"
+        placeholder="Type your activities - Max 50 char"
         onChange={(e) => handleInput(e)}
       />
+
       <select
         name="priority"
         value={input.priority}
