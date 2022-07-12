4b054c7277a560a3de0739e6df75c453725c239e
Alfilister <castillo.c.jm@gmail.com>
2022-07-12 00:14:52 -0500

    Data captured and filtered by date and timeOuts

--- Client/src/Components/DailyTasks.jsx
+++ Client/src/Components/DailyTasks.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import ButtonMain from "./Buttons & Inputs/ButtonMain"
+import TabList from "./TabList"
 
 function DailyTasks() {
   const navigate = useNavigate()
@@ -9,6 +10,16 @@ function DailyTasks() {
 
   const currentDay = new Date()
   const date2Show = currentDay.toISOString().slice(0, 10)
+
+  const timeLimitTask =
+    userInfo && userInfo.tasks.filter((el) => el.timeLimit !== "not-defined")
+
+  const timeOutOnes =
+    timeLimitTask && timeLimitTask.filter((el) => el.timeLimit < date2Show)
+
+  const todayTasks =
+    timeLimitTask && timeLimitTask.filter((el) => el.timeLimit === date2Show)
+  console.log(todayTasks)
 
   return (
     <>
@@ -21,6 +32,16 @@ function DailyTasks() {
           />
           <h2>Hi {userInfo.nickname.toUpperCase()}</h2>
           <h3>Date {date2Show}</h3>
+          <div className="timeOutTasks">
+            {/* {timeOutOnes.map((el) => (
+              <TabList
+                task={el}
+                key={el.task}
+                logginStatus={logginStatus}
+                userRole={userRole}
+              />
+            ))} */}
+          </div>
         </div>
       )}
     </>
