f87d6a44bb8951754bf4917500db618d861ecc68
Alfilister <castillo.c.jm@gmail.com>
2022-07-21 16:52:11 -0500

    Routing for basic users directly to the /home Done

--- Client/src/Pages/Landing.jsx
+++ Client/src/Pages/Landing.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import DailyTasks from "../Components/DailyTasks";
-import Tutorial from "../Components/Tutorial";
+import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import DailyTasks from "../Components/DailyTasks"
+import Tutorial from "../Components/Tutorial"
 
-import firebaseApp from "../Firebase/credenciales";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import { logginStatus, logoutStatus, setUserInfo } from "../Redux/Actions";
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+import firebaseApp from "../Firebase/credenciales"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { getFirestore, doc, getDoc } from "firebase/firestore"
+import { logginStatus, logoutStatus, setUserInfo } from "../Redux/Actions"
+const auth = getAuth(firebaseApp)
+const firestore = getFirestore(firebaseApp)
 
 function Landing() {
-  const logged = useSelector((state) => state.loggin);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const logged = useSelector((state) => state.loggin)
+  const userRole = useSelector((state) => state.userInfo.role)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const getAditionalInfo = async (uid) => {
-    const docuRef = doc(firestore, `/users/${uid}`);
-    const docu = await getDoc(docuRef);
-    const response = docu.data();
-    return response;
-  };
+    const docuRef = doc(firestore, `/users/${uid}`)
+    const docu = await getDoc(docuRef)
+    const response = docu.data()
+    return response
+  }
 
   if (logged) {
     onAuthStateChanged(auth, (firebaseUser) => {
@@ -35,17 +36,30 @@ function Landing() {
               role: info.role,
               tasks: info.tasks,
             })
-          );
-        });
+          )
+        })
 
-        dispatch(logginStatus());
+        dispatch(logginStatus())
       } else {
-        dispatch(logoutStatus());
+        dispatch(logoutStatus())
       }
-    });
+    })
   }
 
-  return <div>{logged ? <DailyTasks /> : <Tutorial />}</div>;
+  function validationRole(userRole) {
+    if (userRole === "basic") {
+      navigate("home")
+    } else if (userRole === "pro") {
+      return <DailyTasks />
+    }
+  }
+
+  return (
+    <div>
+      {!logged && <Tutorial />}
+      {logged && validationRole(userRole)}
+    </div>
+  )
 }
 
-export default Landing;
+export default Landing
