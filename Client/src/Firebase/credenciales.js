c777e4c25d70d3b435db53b9db3f6d2a5289893b
Alfilister <castillo.c.jm@gmail.com>
2022-08-09 23:17:21 -0500

    .env variables protected Firebase

--- Client/src/Firebase/credenciales.js
+++ Client/src/Firebase/credenciales.js
@@ -1,14 +1,23 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_OD,
+} = import.meta.env
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjelUQ9eExcOMKvcnEeUe4aHT2jZiTBnQ",
-  authDomain: "doappauth.firebaseapp.com",
-  projectId: "doappauth",
-  storageBucket: "doappauth.appspot.com",
-  messagingSenderId: "963782832610",
-  appId: "1:963782832610:web:974a83621b1a4eb15949bb",
-};
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_OD,
+}
 
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig)
 
-export default firebaseApp;
+export default firebaseApp
